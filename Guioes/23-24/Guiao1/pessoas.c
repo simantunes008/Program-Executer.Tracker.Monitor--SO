2cfa7b502267cd7ab960906aca2af6141ab9e115
Simão Antunes <simaopfantunes15@duck.com>
2024-02-16 21:26:40 +0000

    Add files via upload

--- Guioes/23-24/Guiao1/pessoas.c
+++ Guioes/23-24/Guiao1/pessoas.c
@@ -10,38 +10,46 @@ typedef struct pessoa {
     int idade;
 } Pessoa;
 
-void adiciona_pessoa(Pessoa * pessoa) {
-    int fd = open("pessoas.bin", O_CREAT | O_WRONLY | O_APPEND, 0644);
-    if (fd < 0) return;
-    write(fd, pessoa, sizeof(Pessoa));
-    close(fd);
+Pessoa * cria_pessoa(char * nome, int idade) {
+    Pessoa * pessoa = malloc(sizeof(Pessoa));
+    strncpy(pessoa -> nome, nome, 200);
+    pessoa -> idade = idade;
+    return pessoa;
 }
 
-void listar_pessoas(int N) {
-    int fd = open("pessoas.bin", O_RDONLY);
-    if (fd < 0) return;
+void adiciona_pessoa(Pessoa * pessoa) {
+    int pessoas_fd = open("pessoas.bin", O_CREAT | O_WRONLY | O_APPEND, 0644);
+    if (pessoas_fd < 0) return;
+    write(pessoas_fd, pessoa, sizeof(Pessoa));
+    close(pessoas_fd);
+}
+
+void lista_pessoas(int N) {
+    int pessoas_fd = open("pessoas.bin", O_RDONLY);
+    if (pessoas_fd < 0) return;
     Pessoa * pessoa = malloc(sizeof(Pessoa));
     int bytes_read = 0;
     char buffer[204];
-    for (int i = 1; read(fd, pessoa, sizeof(Pessoa)) > 0 && i <= N; i++) {
+    for (int i = 1; read(pessoas_fd, pessoa, sizeof(Pessoa)) > 0 && i <= N; i++) {
         bytes_read = snprintf(buffer, 204, "%s %d\n", pessoa->nome, pessoa->idade);
         write(STDOUT_FILENO, buffer, bytes_read);
     }
-    close(fd);
+    close(pessoas_fd);
 }
 
 void altera_idade(Pessoa * nova) {
-    int fd = open("pessoas.bin", O_RDWR, 0644);
-    if (fd < 0) return;
+    int pessoas_fd = open("pessoas.bin", O_RDWR, 0644);
+    if (pessoas_fd < 0) return;
     Pessoa * antiga = malloc(sizeof(Pessoa));
-    while (read(fd, antiga, sizeof(Pessoa)) > 0) {
+    while (read(pessoas_fd, antiga, sizeof(Pessoa)) > 0) {
         if (strcmp(antiga->nome, nova->nome) == 0) {
-            lseek(fd, -204, SEEK_CUR);
-            write(fd, nova, sizeof(Pessoa));
+            lseek(pessoas_fd, -204, SEEK_CUR);
+            write(pessoas_fd, nova, sizeof(Pessoa));
+            free(antiga);
             break;
         }
     }
-    close(fd);
+    close(pessoas_fd);
 }
 
 int main(int argc, char ** argv) {
@@ -51,19 +59,17 @@ int main(int argc, char ** argv) {
     }
 
     if (!strcmp(argv[1], "-i") && argc == 4) {
-        Pessoa * pessoa = malloc(sizeof(Pessoa));
-        strncpy(pessoa -> nome, argv[2], 200);
-        pessoa -> idade = atoi(argv[3]);
+        Pessoa * pessoa = cria_pessoa(argv[2], atoi(argv[3]));
         adiciona_pessoa(pessoa);
+        free(pessoa);
 
     } else if (!strcmp(argv[1], "-l") && argc == 3) {
-        listar_pessoas(atoi(argv[2]));
+        lista_pessoas(atoi(argv[2]));
 
     } else if (!strcmp(argv[1], "-u") && argc == 4) {
-        Pessoa * pessoa = malloc(sizeof(Pessoa));
-        strncpy(pessoa -> nome, argv[2], 200);
-        pessoa -> idade = atoi(argv[3]);
+        Pessoa * pessoa = cria_pessoa(argv[2], atoi(argv[3]));
         altera_idade(pessoa);
+        free(pessoa);
         
     } else {
         write(STDOUT_FILENO, "Opção de utilização inválida!\n", 35);
